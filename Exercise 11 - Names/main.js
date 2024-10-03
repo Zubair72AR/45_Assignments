7aefad9478acd7414e17436777764b616ae0f13d
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-10-03 21:28:38 +0500

    updated

--- Exercise 11 - Names/main.js
+++ Exercise 11 - Names/main.js
@@ -1,3 +1,22 @@
 "use strict";
-// Assignment - 07
-// 
+// Assignment - 11
+// Storing some friends name in the Array
+let names = [
+    "Babar Azam",
+    "Naseem Shah",
+    "Fakhar Zaman",
+    "Muhammad Rizwan",
+    "Azam Khan",
+];
+// LOOP = Printing each Person name
+for (let i = 0; i < names.length; i++) {
+    console.log("Loop:", names[i]);
+}
+// FOR Each = Printing each Person name
+names.forEach((friends) => {
+    console.log("ForEach:", friends);
+});
+// FOR OF LOOP = Printing each Person name
+for (let friendName of names) {
+    console.log("For of Loop:", friendName);
+}
