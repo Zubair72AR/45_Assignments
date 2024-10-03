03dca63cc16234af01b059ff8f89adaea1bf0934
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-10-03 21:45:41 +0500

    updated

--- Exercise 12 - Greetings/main.js
+++ Exercise 12 - Greetings/main.js
@@ -1,3 +1,22 @@
 "use strict";
-// Assignment - 07
-// 
+// Assignment - 12
+// Storing some friends name in the Array
+let friendsName = [
+    "Babar Azam",
+    "Naseem Shah",
+    "Fakhar Zaman",
+    "Muhammad Rizwan",
+    "Azam Khan",
+];
+// LOOP = Printing Message for each Person
+for (let i = 0; i < friendsName.length; i++) {
+    console.log(`Hallo ${friendsName[i]}, How are you doing today?`);
+}
+// FOR Each = Printing Message for each Person
+friendsName.forEach((friends) => {
+    console.log(`Assalam-o-Alaikum ${friends}, will you attend today's class?`);
+});
+// FOR OF LOOP = Printing Message for each Person
+for (let nameOfFriends of friendsName) {
+    console.log(`Hi ${nameOfFriends}, Today is my Birthday....!`);
+}
