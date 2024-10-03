d47495169c15d90411eece11cb0179d542f2ac7a
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-10-03 22:04:45 +0500

    updated

--- Exercise 13 - Your Own Array/main.ts
+++ Exercise 13 - Your Own Array/main.ts
@@ -1,3 +1,26 @@
-// Assignment - 07
+// Assignment - 13
 
-// 
+// Storing Mode of Transport name in the Array
+let modeOfTransportation: string[] = ["Cycle", "Plane", "Bus", "Bike", "Car"];
+
+// Printing Message
+console.log(`I would like to own a ${modeOfTransportation[0]}.`);
+console.log(`I would like to own a ${modeOfTransportation[1]}.`);
+console.log(`I would like to own a ${modeOfTransportation[2]}.`);
+console.log(`I would like to own a ${modeOfTransportation[3]}.`);
+console.log(`I would like to own a ${modeOfTransportation[4]}.`);
+
+// LOOP = Printing Message
+for (let i = 0; i < modeOfTransportation.length; i++) {
+  console.log(`I would like to own a ${modeOfTransportation[i]}.`);
+}
+
+// FOR Each = Printing Message
+modeOfTransportation.forEach((transport) => {
+  console.log(`I would like to own a ${transport}.`);
+});
+
+// FOR OF LOOP = Printing Message
+for (let typeOfTransport of modeOfTransportation) {
+  console.log(`I would like to own a ${typeOfTransport}.`);
+}
