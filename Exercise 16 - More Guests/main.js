358a015835fca34ae58eda32d6ddcdbb78bdcd28
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-10-04 11:11:45 +0500

    updated 16

--- Exercise 16 - More Guests/main.js
+++ Exercise 16 - More Guests/main.js
@@ -1,3 +1,21 @@
 "use strict";
-// Assignment - 07
-// 
+// Assignment - 16
+// Storing Guest in the Array
+let guestList3 = [
+    "Aman Ali",
+    "Amir Khan",
+    "Ikram Jan",
+    "Nadeem Bhai",
+];
+// Announcing that i found a bigger table
+console.log("Hurry! we have found a bigger table");
+// UNSHIFT - Adding new Guest to the beginning of Array
+guestList3.unshift("Imran Khan");
+// SPLICE - Adding one new guest to the middle of array
+guestList3.splice(Math.floor(guestList3.length / 2), 0, "Zubair AR");
+// PUSH - Adding new Guest to the end array
+guestList3.push("Afsar Khan");
+// FOR Each = Printing Invitation to all Guest
+guestList3.forEach((newGuests) => {
+    console.log(`Dear ${newGuests} you are invited to Dinner.`);
+});
