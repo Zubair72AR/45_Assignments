b07680225188d52f39eef54611adf63ac9b7eb33
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-10-06 12:49:01 +0500

    updated

--- Exercise 45 - Cars/main.js
+++ Exercise 45 - Cars/main.js
@@ -1,3 +1,11 @@
 "use strict";
-// Assignment - 07
-// 
+// Assignment - 45
+// function
+function create_car(manufacturer, model, ...option) {
+    let car = { manufacturer, model };
+    option.forEach(([key, value]) => (car[key] = value));
+    return car;
+}
+// Example usage
+let my_car01 = create_car("Toyota", "Corolla", ["color", "white"], ["year", 2014]);
+console.log(my_car01);
--- Exercise 45 - Cars/main.ts
+++ Exercise 45 - Cars/main.ts
@@ -1,3 +1,28 @@
-// Assignment - 07
+// Assignment - 45
 
-// 
+// Define Type for the CarInfo function
+type CarInfo = {
+  manufacturer: string;
+  model: string;
+  [key: string]: string | number;
+};
+// function
+function create_car(
+  manufacturer: string,
+  model: string,
+  ...option: [string, string | number][]
+): CarInfo {
+  let car: CarInfo = { manufacturer, model };
+  option.forEach(([key, value]) => (car[key] = value));
+  return car;
+}
+
+// Example usage
+let my_car01 = create_car(
+  "Toyota",
+  "Corolla",
+  ["color", "white"],
+  ["year", 2014]
+);
+
+console.log(my_car01);
